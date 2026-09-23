33b66f2349ae44bc8ea7b85d1aacea9a396b6de3
Juan Camilo Martinez <juankmilo0014@gmail.com>
2026-09-22 21:41:50 -0500

    tags

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -2,6 +2,7 @@ package conduitApp;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,8 @@ class ConduitTest {
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:conduitApp")
-                //.outputCucumberJson(true)
+                // .outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
-
 }
